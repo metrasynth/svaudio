7372972740102bbc698cdc96180e7cbb4d6eb7f3
Matthew Scott <matt@11craft.com>
2021-04-10 00:01:24 -0500

    Adds production config for sending email

--- config/settings/production.py
+++ config/settings/production.py
@@ -98,6 +98,12 @@ INSTALLED_APPS += ["anymail"]  # noqa F405
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 # https://anymail.readthedocs.io/en/stable/esps
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("DJANGO_EMAIL_HOST")
+EMAIL_HOST_USER = env("DJANGO_EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("DJANGO_EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = env("DJANGO_EMAIL_USE_TLS")
+EMAIL_PORT = env("DJANGO_EMAIL_PORT")
+
 ANYMAIL = {}
 
 # django-compressor
