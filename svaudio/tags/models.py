c2f3d14784a2f96cdf976f5f899e955fb98b7679
Matthew Scott <matt@11craft.com>
2021-04-19 09:45:46 -0500

    Adds field for keeping track of who added tags to an item

--- svaudio/tags/models.py
+++ svaudio/tags/models.py
@@ -1,6 +1,8 @@
 from django.db import models as m
 from taggit.models import GenericTaggedItemBase, TagBase
 
+from svaudio.users.models import User
+
 
 class Tag(TagBase):
     pass
@@ -8,5 +10,14 @@ class Tag(TagBase):
 
 class TaggedItem(GenericTaggedItemBase):
     tag = m.ForeignKey(
-        Tag, on_delete=m.CASCADE, related_name="%(app_label)s_%(class)s_items"
+        Tag,
+        on_delete=m.CASCADE,
+        related_name="%(app_label)s_%(class)s_items",
+    )
+    added_by = m.ForeignKey(
+        User,
+        on_delete=m.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="tags_added",
     )
