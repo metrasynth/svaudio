c1e63fb24ec2ddd2131332fbb0c2021a2417599e
Matthew Scott <matt@11craft.com>
2021-04-22 13:17:19 -0500

    Fixes template name for case-sensitive filesystems

--- svaudio/tags/views.py
+++ svaudio/tags/views.py
@@ -6,6 +6,7 @@ from . import models as m
 class TagListView(ListView):
 
     model = m.Tag
+    template_name = "tags/tag_list.html"
 
 
 tag_list_view = TagListView.as_view()
@@ -14,6 +15,7 @@ tag_list_view = TagListView.as_view()
 class TagDetailView(DetailView):
 
     model = m.Tag
+    template_name = "tags/tag_detail.html"
 
 
 tag_detail_view = TagDetailView.as_view()
