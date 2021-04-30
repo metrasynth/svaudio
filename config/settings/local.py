2cacb44cd62fb25943d3bbc738fdb4040946fd00
Matthew Scott <matt@11craft.com>
2021-04-30 14:55:55 -0500

    Adds config for running dev environment in docker

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="JKJdZxoH54uQvuoWDFXAjoNhsN1PktoWK1N2rTFwpY9cP0oKOGdUB8vHpXjVcPxo",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "django"]
 
 # CACHES
 # ------------------------------------------------------------------------------
