80e3bd915a640bcf306504a760823f3ffa76fa26
Matthew Scott <matt@11craft.com>
2021-05-07 17:58:42 -0500

    gettext_lazy ← ugettext_lazy (deprecated)

--- svaudio/users/tests/test_forms.py
+++ svaudio/users/tests/test_forms.py
@@ -2,7 +2,7 @@
 Module for all Form Tests.
 """
 import pytest
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from svaudio.users.forms import UserCreationForm
 from svaudio.users.models import User
